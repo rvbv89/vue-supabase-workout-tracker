e053a16b6c4293a9fd182dc7fda982bc1c4e0107
rvbv89 <garzotto5389@gmail.com>
2021-12-13 12:35:33 -0600

    dump

---
+++ src/router/index.js
@@ -0,0 +1,40 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import Login from "../views/Login.vue"
+import Register from "../views/Register.vue"
+import Create from "../views/Create.vue"
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },
+  {
+    path:"/register",
+    name: "Register",
+    component: Register,
+  },
+  {
+    path:"/create",
+    name: "Create",
+    component: Create,
+  }
+
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+// Change document titles
+
+// Route guard for auth routes
+
+export default router;
---
+++ src/views/Create.vue
@@ -0,0 +1,152 @@
+<template>
+  <div class="max-w-screen-md mx-auto px-4 py-10">
+    <!-- Status Message -->
+    <div
+      v-if="statusMsg || errorMsg"
+      class="mb-10 p-4 bg-light-grey rounded-md shadow-lg"
+    >
+      <p class="text-at-light-green">{{ statusMsg }}</p>
+      <p class="text-red-500">{{ errorMsg }}</p>
+    </div>
+
+    <!-- create -->
+    <div class="p-8 flex items-start bg-light-grey rounded-md shadow-lg">
+      <form class="flex flex-col gap-y-5 w-full">
+        <h1 class="text-2xl text-at-light-green">Record Workout</h1>
+
+        <!-- workout name -->
+        <div class="flex flex-col">
+          <label for="workout-name" class="mb-1 text-sm text-at-light-green"
+            >Workout Name</label
+          >
+          <input
+            type="text"
+            required
+            class="p-2 text-gray-500 focus:outline-none"
+            id="workout-name"
+            v-model="workoutName"
+          />
+        </div>
+        <!-- workout type -->
+        <div class="flex flex-col">
+          <label for="workout-tyoe" class="mb-1 text-sm text-at-light-green"
+            >Workout Type</label
+          >
+          <select
+            id="workout-type"
+            class="p-2 text-gray-500 focus:outline-none"
+            required
+            v-model="workoutType"
+          >
+            <option value="select-workout">Select Workout</option>
+            <option value="strength">Strength Training</option>
+            <option value="cardio">Cardio</option>
+          </select>
+        </div>
+        <!-- strength training inputs -->
+        <div v-if="workoutType === 'strength'" class="flex flex-col gap-y-4">
+          <div
+            class="flex flex-col gap-x-6 gap-y-2 relative md:flex-row"
+            v-for="(item, index) in exercises"
+            :key="index"
+          >
+            <div class="flex flex-col md:1/3">
+              <label
+                for="exercise-name"
+                class="mb-1 text-sm text-at-light-green"
+                >Exercise</label
+              >
+              <input
+                class="p-2 w-full text-gray-500 focus:outline-none"
+                required
+                type="text"
+                v-model="item.exercise"
+              />
+            </div>
+            <div class="flex flex-col flex-1">
+              <label for="sets" class="mb-1 text-sm text-at-light-green"
+                >Sets</label
+              >
+              <input
+                class="p-2 w-full text-gray-500 focus:outline-none"
+                required
+                type="text"
+                v-model="item.sets"
+              />
+            </div>
+            <div class="flex flex-col flex-1">
+              <label for="reps" class="mb-1 text-sm text-at-light-green"
+                >Reps</label
+              >
+              <input
+                class="p-2 w-full text-gray-500 focus:outline-none"
+                required
+                type="text"
+                v-model="item.reps"
+              />
+            </div>
+            <div class="flex flex-col flex-1">
+              <label for="weight" class="mb-1 text-sm text-at-light-green"
+                >Weight (lbs)</label
+              >
+              <input
+                class="p-2 w-full text-gray-500 focus:outline-none"
+                required
+                type="text"
+                v-model="item.weight"
+              />
+            </div>
+            <img
+              src="@/assets/images/trash-light-green.png"
+              class="h-4 w-auto absolute -left-5 cursor-pointer"
+              alt=""
+            />
+          </div>
+          <button
+            type="button"
+            class="
+              mt-6
+              py-2
+              px-6
+              rounded-sm
+              self-start
+              text-sm text-white
+              bg-at-light-green
+              duration-200
+              border-solid border-2 border-transparent
+              hover:border-at-light-green
+              hover:bg-white
+              hover:text-at-light-green
+            "
+          >
+            Add Exercise
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  name: "create",
+  setup() {
+    // Create data
+    const workoutName = ref("");
+    const workoutType = ref("select-workout");
+    const exercises = ref([1]);
+    const statusMsg = ref(null);
+    const errorMsg = ref(null);
+    // Add exercise
+
+    // Delete exercise
+
+    // Listens for chaging of workout type input
+
+    // Create workout
+
+    return { workoutName, workoutType, exercises, statusMsg, errorMsg };
+  },
+};
+</script>
